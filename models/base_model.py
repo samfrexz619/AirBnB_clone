1533f1efa900cf5289f8a921293166026b863c0f
BlessingOse <maryblessing633@gmail.com>
2023-07-10 06:36:25 -0700

    docstrings added

--- models/base_model.py
+++ models/base_model.py
@@ -6,9 +6,9 @@ from datetime import datetime
 from models import storage
 
 class BaseModel:
-    ''' '''
+    '''Defines all common attributes/methods '''
     def __init__(self, *args, **kwargs):
-        ''' '''
+        '''initializes all attributes '''
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -24,18 +24,19 @@ class BaseModel:
 
 
     def __str__(self):
-        ''' '''
+        '''returns class name, id and attribute dictionary '''
         class_name = '[" + self.__class__.__name__ + "]'
         dic = {k: v for (k, v) in self.__dict__.items() if (not v) is False}
         return class_name + ' (" + self.id + ") ' + str(dic)
 
     def save(self):
-        ''' '''
+        '''updates last update time '''
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        ''' '''
+        '''creates a new dictionary, adding a key and returning
+        datemtimes converted to strings '''
         n_dict = {}
 
         for key, vals in self.__dict__.items():
