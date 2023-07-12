fca4989b721377a0b89b2d70788d6ac67fa663dc
BlessingOse <maryblessing633@gmail.com>
2023-07-12 05:42:10 -0700

    docstrings added

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-''' '''
+''' Unittest for amenity.py '''
+
 import unittest
 from models.amenity import Amenity
 import datetime
 
 class TestAmenity(unittest.TestCase):
-    ''' '''
+    ''' Tests instances and methods from amenity class '''
     aty = Amenity()
 
     def test_class_exists(self):
-        ''' '''
+        ''' tests if class exists '''
         result = "<class 'models.amenity.Amenity'>"
         self.assertEqual(str(type(self.aty)), result)
 
     def test_user_inheritance(self):
-        ''' '''
+        ''' test if Amenity is a subclass of BaseModel '''
         self.assertIsInstance(self.aty, Amenity)
 
     def test_has_attributes(self):
-        ''' '''
+        ''' verify if attributes exist '''
         self.assertTrue(hasattr(self.aty, 'name'), True)
 
     def test_types(self):
-        ''' '''
+        ''' tests if the type of the attribute is the correct one '''
         self.assertIsInstance(self.aty.name, str)
 
 if __name__ == '__main__':
