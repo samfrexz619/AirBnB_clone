81aab6ec02eed15dad7259e335e677a892efe6f3
BlessingOse <maryblessing633@gmail.com>
2023-07-12 05:19:55 -0700

    docstrings added

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-''' '''
+''' Unittest for user.py '''
 import unittest
 from models.user import User
 import datetime
 
 
 class UserCase(unittest.TestCase):
-    ''' '''
+    '''Tests instances and methods from user class '''
     my_user = User()
 
     def test_class_exists(self):
-        ''' '''
+        '''tests if class exists '''
         self.assertEqual(str(type(self.my_user)), "<class 'models.user.User'>")
 
     def test_user_inheritance(self):
-        ''' '''
+        '''test if User is a subclass of BaseModel '''
         self.assertIsInstance(self.my_user, User)
 
     def test_has_attributes(self):
-        ''' '''
+        '''verify if attributes exist '''
         self.assertTrue(hasattr(self.my_user, 'email'))
         self.assertTrue(hasattr(self.my_user, 'password'))
         self.assertTrue(hasattr(self.my_user, 'first_name'))
@@ -28,7 +28,7 @@ class UserCase(unittest.TestCase):
         self.assertTrue(hasattr(self.my_user, 'updated_at'))
 
     def test_types(self):
-        ''' '''
+        '''tests if the type of the attribute is the correct one '''
         self.assertIsInstance(self.my_user.first_name, str)
         self.assertIsInstance(self.my_user.last_name, str)
         self.assertIsInstance(self.my_user.email, str)
