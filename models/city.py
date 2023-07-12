55839949f7a510c4519cc81837273f4b675564dd
BlessingOse <maryblessing633@gmail.com>
2023-07-12 05:57:53 -0700

    docstrings added

--- models/city.py
+++ models/city.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-''' '''
+''' Defines City '''
+
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    ''' '''
+    ''' defines city to look for '''
     state_id = ''
     name = ''
