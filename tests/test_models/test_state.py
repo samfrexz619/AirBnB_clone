b76d8aabef27ae95de1f73720d7953ad49961f34
BlessingOse <maryblessing633@gmail.com>
2023-07-12 05:23:48 -0700

    docstrings added

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-''' '''
+''' Unittest for amenity.py '''
 import unittest
 from models.state import State
 import datetime
 
 
 class TestState(unittest.TestCase):
-    ''' '''
+    ''' Tests instances and methods from State class '''
     st = State()
 
     def test_class_exists(self):
-        ''' '''
+        ''' tests if class exists '''
         result = "<class 'models.state.State'>"
         self.assertEqual(str(type(self.st)), result)
 
     def test_user_inheritance(self):
-        ''' '''
+        ''' test if State is a subclass of BaseModel '''
         self.assertIsInstance(self.st, State)
 
     def testHasAttributes(self):
@@ -26,7 +26,7 @@ class TestState(unittest.TestCase):
         self.assertTrue(hasattr(self.st, 'updated_at'))
 
     def test_types(self):
-        ''' '''
+        ''' verify if attributes exist '''
         self.assertIsInstance(self.st.name, str)
         self.assertIsInstance(self.st.id, str)
         self.assertIsInstance(self.st.created_at, datetime.datetime)
